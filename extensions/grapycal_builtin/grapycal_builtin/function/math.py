05f93ed674b53ecc6720cab955c736ab65111d67
JimmyHu <60071jimmy@gmail.com>
2025-09-20 00:37:25 +0800

    Update math.py
    Update math.py

--- extensions/grapycal_builtin/grapycal_builtin/function/math.py
+++ extensions/grapycal_builtin/grapycal_builtin/function/math.py
@@ -313,6 +313,7 @@ class SecNode(FunctionNode):
     category = 'function/math'
     inputs = ['a']
     max_in_degree = [None]
+    outputs = ['sec(a)']
 class AbsNode(FunctionNode):
     '''
     Calculate abs(sum(`A`))
